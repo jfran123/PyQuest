ab6fe909b0ddb9376bfb0cdab93768941b4a9e83
jeffinfrancis <jeffin2202@gmail.com>
2026-03-22 15:03:14 -0400

    PyQuest Initial Checkin

--- init_db.py
+++ init_db.py
@@ -4,48 +4,50 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-fullname TEXT,
-email TEXT UNIQUE,
-password TEXT,
-role TEXT,
-points INTEGER DEFAULT 0
-)
-""")
+# c.execute("""
+# CREATE TABLE IF NOT EXISTS users(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# fullname TEXT,
+# email TEXT UNIQUE,
+# password TEXT,
+# role TEXT,
+# points INTEGER DEFAULT 0
+# )
+# """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS problems(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-description TEXT,
-level INTEGER
-)
-""")
+# c.execute("""
+# CREATE TABLE IF NOT EXISTS problems(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# title TEXT,
+# description TEXT,
+# level INTEGER
+# )
+# """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS testcases(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-problem_id INTEGER,
-input TEXT,
-expected_output TEXT
-)
-""")
+# c.execute("""
+# CREATE TABLE IF NOT EXISTS testcases(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# problem_id INTEGER,
+# input TEXT,
+# expected_output TEXT
+# )
+# """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS submissions(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user TEXT,
-problem_id INTEGER,
-code TEXT,
-result TEXT
-)
-""")
+# c.execute("""
+# CREATE TABLE IF NOT EXISTS submissions(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# user TEXT,
+# problem_id INTEGER,
+# code TEXT,
+# result TEXT
+# )
+# """)
 
-c.execute("""insert into users(fullname,email,password,role) values('Admin','admin@admin.com','admin123','admin')""")
-c.execute("ALTER TABLE problems ADD COLUMN teacher_id INTEGER")
+#c.execute("""insert into users(fullname,email,password,role) values('Admin','admin@admin.com','admin123','admin')""")
+#c.execute("ALTER TABLE problems ADD COLUMN teacher_id INTEGER")
 # c.execute("update problems set teacher_id=2")
+c.execute("update users set fullname='Ivan', email='ivan@gmail.com' where id=2")
+c.execute("update users set email='jeffin@gmail.com' where id=1")
 conn.commit()
 conn.close()
 
